72d6f08b0f204721b9ae2349fa20c52c6af88d08
Gabriel Pérez <gabrielperezlago.dev@gmail.com>
2025-10-08 16:00:05 +0200

    25.10.08 Algoritmos Listos

---
+++ src/com/gabriel/algoritmos/Algoritmos.java
@@ -0,0 +1,71 @@
+package com.gabriel.algoritmos;
+
+public class Algoritmos extends Thread {
+    public int num;
+    public int[] arr;
+
+    Algoritmos(int num, int[] arr) {
+        this.num = num;
+    }
+
+
+    @Override
+    public void run() {
+        switch(num){
+            case 1: bubbleSort(arr);
+            break;
+            case 2: selectionSort(arr);
+            break;
+            case 3: insertionSort(arr);
+            break;
+        }
+    }
+
+    public static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        boolean swapped;
+        for (int i = 0; i < n - 1; i++) {
+            swapped = false;
+            for (int j = 0; j < n - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+            if (!swapped) {
+                break;
+            }
+        }
+    }
+
+    public static void selectionSort(int[] arr) {
+        int n = arr.length;
+
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
+    public static void insertionSort(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            int clave = arr[i];
+            int j = i - 1;
+            while (j >= 0 && arr[j] > clave) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = clave;
+        }
+    }
+}
